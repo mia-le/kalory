42746145daf2318ab455a527bb053dab81a2f240
Mia Le <leanh@grinnell.edu>
2020-09-14 17:11:20 +0000

    HW#5

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -31,5 +31,14 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "title", "Roll a Dice"
   end
-
+  
+  test "dice results are integers" do
+    get roll_path
+    assert_response :success
+    assert_not_nil @controller.view_assigns['dice1_result']
+    assert_not_nil @controller.view_assigns['dice2_result']
+    assert_instance_of Integer, @controller.view_assigns['dice1_result']
+    assert_instance_of Integer, @controller.view_assigns['dice2_result']
+end
+  
 end
