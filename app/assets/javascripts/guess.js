6bf5af799292310cdd5226d67a33e603c996a8a8
Mia Le <leanh@grinnell.edu>
2020-10-05 17:55:07 +0000

    Implemented the input and check for Calories

--- app/assets/javascripts/guess.js
+++ app/assets/javascripts/guess.js
@@ -1,6 +1,14 @@
 let is_loading = false;
 const upc_string ='857913004041 688267142840 657082002844 51600080015 78742088679 811392020275 612781101908 90939132019 60522000087 63100109578 846036003611 70253267345 16000296527 32712124 68100896503 41268188703 20000277967'
-const upc_list = upc_string.split(" ")
+const upc_list = upc_string.split(" ");
+
+function diu(thing) {
+  if (thing === null || thing === undefined) {
+    return `Thing ${Math.round(Math.random() * 10000)}`
+  }
+  
+  return thing;
+}
 
 function load_new_item() {
   if (is_loading)
@@ -13,32 +21,33 @@ function load_new_item() {
   //for (let i = 0; i < upc_list.length; i++) {
     // let random_upc= upc_list[i] s
     //17-27
-    getDetails(random_upc, function(data) {
-    //     console.log(`${i+1} at ${data["item_name"]}`);
+  getDetails(random_upc, function(data) {
+  //     console.log(`${i+1} at ${data["item_name"]}`);
+  
+    // Load in details to DOM
+    document.getElementById('display_food-name').innerHTML = diu(data["item_name"]);
     
-      // Load in details to DOM
-      document.getElementById('display_food-name').innerHTML = data["item_name"];
-      document.getElementById('display_brand-name').innerHTML = data["brand_name"];
-      document.getElementById('display_serving-per-container').innerHTML =`Serving size: ${data["nf_servings_per_container"]} x ` ;
-      document.getElementById('display_serving-quantity').innerHTML = data["nf_serving_size_qty"];
-      document.getElementById('display_serving-unit').innerHTML = data["nf_serving_size_unit"];
-      document.getElementById('display_food-description').innerHTML = data["nf_ingredient_statement"];
-      document.getElementById('display_calories-label').innerHTML = "Calories: ";
-      
-      let calories = 11;
-      document.getElementById('display_calories-input').setAttribute("answer", calories);
-      document.getElementById('display_calories-input').style.visibility = "visible";
-      is_loading = false;
-      document.getElementById('btn_load-new').innerHTML = "Load New Item";
-    });
+    document.getElementById('display_brand-name').innerHTML = data["brand_name"];
+    document.getElementById('display_serving-per-container').innerHTML =`Serving size: ${data["nf_servings_per_container"]} x ` ;
+    document.getElementById('display_serving-quantity').innerHTML = data["nf_serving_size_qty"];
+    document.getElementById('display_serving-unit').innerHTML = data["nf_serving_size_unit"];
+    document.getElementById('display_food-description').innerHTML = data["nf_ingredient_statement"];
+    document.getElementById('display_calories-label').innerHTML = "Calories: ";
+    
+    let calories = 11;
+    document.getElementById('display_calories-input').setAttribute("answer", calories);
+    document.getElementById('display_calories-input').style.visibility = "visible";
+    is_loading = false;
+    document.getElementById('btn_load-new').innerHTML = "Load New Item";
+  });
   //}
   
   // hi baby. im ask u, if it return when is_loading === false, when it work? since is_loading = true always after it
 }
 
 function check_answer() {
-	let answer = parseInt(document.getElementById('display_calories').getAttribute("answer"));
-	let guess = parseInt(document.getElementById('display_calories').value);
+	let answer = parseInt(document.getElementById('display_calories-input').getAttribute("answer"));
+	let guess = parseInt(document.getElementById('display_calories-input').value);
 	
 	console.log(answer);
 	
@@ -64,7 +73,7 @@ const getDetails = function(upc, callback) {
   });
 }
 
-document.addEventListener('DOMContentFullyLoaded', (event) => {
+document.addEventListener('DOMContentLoaded', (event) => {
   load_new_item();
 });
 
